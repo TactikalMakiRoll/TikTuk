12bad538c85bfe2f51b18b3e1203e463ba96d290
TactikalMakiRoll <sagridin@gmail.com>
2021-11-17 13:26:26 +0200

    change UserInfo call to a real one (was testing errors)

--- src/fetchUserInfo.js
+++ src/fetchUserInfo.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // API call для получения подробной информации о пользователе
 export default async function fetchUserInfo(_user){
     try{
-        const response = await axios.get ('https://tiktok33.p.rapidapid.com/user/info/' + _user,{
+        const response = await axios.get ('https://tiktok33.p.rapidapi.com/user/info/' + _user,{
             headers: {
                 'x-rapidapi-host': 'tiktok33.p.rapidapi.com',
                 'x-rapidapi-key': 'c1257dc04cmshd888bbb072eb770p1f2b8ajsnbf16d4cd1d66'
